258c3cb10a217f2ee1c849ba35a44f00de2bc170
Frank Blau <fjblau@gmail.com>
2025-12-29 13:17:56 +0100

    Implementation

--- kessler-game/src/components/ControlPanel/BudgetGauge.tsx
+++ kessler-game/src/components/ControlPanel/BudgetGauge.tsx
@@ -19,6 +19,13 @@ export function BudgetGauge({ budget, maxBudget = 200_000_000 }: BudgetGaugeProp
           className={`h-full transition-all duration-300 ${getColorClass()} absolute left-0 top-0`}
           style={{ width: `${percentage}%` }}
         />
+        {[16.67, 33.33, 50, 66.67, 83.33].map((pos, i) => (
+          <div
+            key={i}
+            className="absolute top-0 h-full w-px bg-slate-900 z-[5]"
+            style={{ left: `${pos}%` }}
+          />
+        ))}
         <span className="relative z-10 text-sm font-medium text-black">BUDGET REMAINING</span>
       </div>
     </div>
